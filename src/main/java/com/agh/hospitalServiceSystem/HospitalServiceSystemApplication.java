deb59aedc455356d7b0eef3f6e4b44b83a2e3c35
Wiktor <Wiktor@192.168.0.24>
2020-10-20 14:30:38 +0200

    Initial Commit

---
+++ src/main/java/com/agh/hospitalServiceSystem/HospitalServiceSystemApplication.java
@@ -0,0 +1,13 @@
+package com.agh.hospitalServiceSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalServiceSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalServiceSystemApplication.class, args);
+	}
+
+}
